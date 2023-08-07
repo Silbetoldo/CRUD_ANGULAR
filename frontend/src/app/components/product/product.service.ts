39e4bf19639b9c692a274326d86f2aa8e182fc04
Silvana Caldeira Miranda Betoldo <32404677+Silbetoldo@users.noreply.github.com>
2023-08-07 11:56:16 -0300

    Exibindo Produtos

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {MatSnackBar  } from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Product } from './product-create/product.model';
 import { Observable } from 'rxjs';
 
@@ -13,21 +13,21 @@ export class ProductService {
   constructor(private snakbar: MatSnackBar, private http: HttpClient) { }
 
   //Exibe mensagem ao clicar em botão Salvar
-   shoMessage(msg: string): void{
-         this.snakbar.open(msg, 'X', {
-          duration: 3000,
-          horizontalPosition: "right",
-          verticalPosition: "top"
-         })
-   }
+  shoMessage(msg: string): void {
+    this.snakbar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    })
+  }
 
-   //Chamada para o back-end - Inserindo produtos 
-   create(product: Product): Observable<Product>{
-     return this.http.post<Product>(this.baseUrl, product )
-   }
-//Listando produtos
-   read(): Observable<Product[]>{
+  //Chamada para o back-end - Inserindo produtos 
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product)
+  }
+  //Listando produtos
+  read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl)
-   }
+  }
 
 }
